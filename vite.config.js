b668be0ca35c8d24ed6946b700d69117b92176cd
Kairung Vangmanaw <94585323+VangmanawKairung@users.noreply.github.com>
2026-08-27 18:59:04 +0700

    Fix base path quotes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nft-prview-card-component/",
+  base: '/nft-prview-card-component/',
 });
